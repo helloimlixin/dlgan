2cea5fa28882d9ddcbd829aa5b7ca549734b5e7b
Xin Li <helloimlixin@gmail.com>
2024-01-20 12:46:43 -0500

    Added the decoder implementation.

--- models/decoder.py
+++ models/decoder.py
@@ -1,11 +1,10 @@
-#==============================================================================
-# Description: This file defines the encoder module of the model.
+#  ==============================================================================
+#  Description: This file defines the decoder module of the model.
+#  Copyright (C) 2024 Xin Li
 #
 # References:
 #   - Van Den Oord, A., & Vinyals, O. (2017). Neural discrete representation learning.
 #       Advances in neural information processing systems, 30.
-#
-#  Copyright (C) 2024 Xin Li
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -18,30 +17,18 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-#==============================================================================
+#  ==============================================================================
 
 import torch.nn as nn
 import torch.nn.functional as F
 from .helpers import ResidualStack
 
-class Encoder(nn.Module):
-    ''''Encoder module of the model.'''
+class Decoder(nn.Module):
+    '''Decoder module of the model.'''
     def __init__(self, in_channels, num_hiddens, num_residual_layers, num_residual_hiddens):
-        super(Encoder, self).__init__()
+        super(Decoder, self).__init__()
 
         self._conv_1 = nn.Conv2d(in_channels=in_channels,
-                                 out_channels=num_hiddens//2,
-                                 kernel_size=4,
-                                 stride=2,
-                                 padding=1)
-
-        self._conv_2 = nn.Conv2d(in_channels=num_hiddens//2,
-                                 out_channels=num_hiddens,
-                                 kernel_size=4,
-                                 stride=2,
-                                 padding=1)
-
-        self._conv_3 = nn.Conv2d(in_channels=num_hiddens,
                                  out_channels=num_hiddens,
                                  kernel_size=3,
                                  stride=1,
@@ -52,8 +39,22 @@ class Encoder(nn.Module):
                                              num_residual_layers=num_residual_layers,
                                              num_residual_hiddens=num_residual_hiddens)
 
+        self._conv_trans_1 = nn.ConvTranspose2d(in_channels=num_hiddens,
+                                               out_channels=num_hiddens//2,
+                                               kernel_size=4,
+                                               stride=2,
+                                               padding=1)
+
+        self._conv_trans_2 = nn.ConvTranspose2d(in_channels=num_hiddens//2,
+                                               out_channels=3,
+                                               kernel_size=4,
+                                               stride=2,
+                                               padding=1)
+
     def forward(self, x):
-        x = F.relu(self._conv_1(x))
-        x = F.relu(self._conv_2(x))
-        x = self._conv_3(x)
-        return self._residual_stack(x)
+        x = self._conv_1(x)
+        x = self._residual_stack(x)
+        x = self._conv_trans_1(x)
+        x = F.relu(x)
+
+        return self._conv_trans_2(x)
