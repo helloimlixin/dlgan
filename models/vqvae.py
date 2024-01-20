eccfe99216c08f37d74e22375dd786ec84fe6f48
Xin Li <helloimlixin@gmail.com>
2024-01-20 13:32:54 -0500

    Added the vanilla vqvae implementation.

---
+++ models/vqvae.py
@@ -0,0 +1,66 @@
+#  ==============================================================================
+#  Description: Helper functions for the model.
+#  Copyright (C) 2024 Xin Li
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#  ==============================================================================
+
+import torch.nn as nn
+import torch.nn.functional as F
+from .encoder import Encoder
+from .decoder import Decoder
+from .vqvae import VectorQuantizer
+
+class VQVAE(nn.Module):
+    '''VQ-VAE model.'''
+
+    def __init__(self, in_channels, num_hiddens, num_residual_layers, num_residual_hiddens, num_embeddings,
+                 embedding_dim, commitment_cost, epsilon=1e-10, decay=0):
+        super(VQVAE, self).__init__()
+
+        self._encoder = Encoder(in_channels=in_channels,
+                                num_hiddens=num_hiddens,
+                                num_residual_layers=num_residual_layers,
+                                num_residual_hiddens=num_residual_hiddens)
+
+        self._pre_vq_conv = nn.Conv2d(in_channels=num_hiddens,
+                                      out_channels=embedding_dim,
+                                      kernel_size=1,
+                                      stride=1)
+
+        # if decay > 0.0:
+        #     self._vq_vae = VectorQuantizerEMA(num_embeddings=num_embeddings,
+        #                                       embedding_dim=embedding_dim,
+        #                                       commitment_cost=commitment_cost,
+        #                                       decay=decay)
+        # else:
+        #     self._vq_vae = VectorQuantizer(num_embeddings=num_embeddings,
+        #                                    embedding_dim=embedding_dim,
+        #                                    commitment_cost=commitment_cost)
+
+        self._vq_bottleneck = VectorQuantizer(num_embeddings=num_embeddings,
+                                       embedding_dim=embedding_dim,
+                                       commitment_cost=commitment_cost)
+
+        self._decoder = Decoder(in_channels=embedding_dim,
+                                num_hiddens=num_hiddens,
+                                num_residual_layers=num_residual_layers,
+                                num_residual_hiddens=num_residual_hiddens)
+
+    def forward(self, x):
+        z = self._encoder(x)
+        z = self._pre_vq_conv(z)
+        loss, quantized, perplexity, encodings = self._vq_bottleneck(z)
+        x_recon = self._decoder(quantized)
+
+        return loss, x_recon, perplexity, encodings
