f6102e87f4e38ed7b7ec01fbe763ed75e95669d0
Xin Li <helloimlixin@gmail.com>
2024-01-20 12:35:37 -0500

    Added the encoder implementation.

--- models/encoder.py
+++ models/encoder.py
@@ -1,3 +1,9 @@
+# This file defines the encoder module of the model.
+#
+# References:
+#   - Van Den Oord, A., & Vinyals, O. (2017). Neural discrete representation learning.
+#       Advances in neural information processing systems, 30.
+#
 #  Copyright (C) 2024 Xin Li
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,3 +17,41 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
+import torch.nn as nn
+import torch.nn.functional as F
+from .helpers import ResidualStack
+
+class Encoder(nn.Module):
+    ''''Encoder module of the model.'''
+    def __init__(self, in_channels, num_hiddens, num_residual_layers, num_residual_hiddens):
+        super(Encoder, self).__init__()
+
+        self._conv_1 = nn.Conv2d(in_channels=in_channels,
+                                 out_channels=num_hiddens//2,
+                                 kernel_size=4,
+                                 stride=2,
+                                 padding=1)
+
+        self._conv_2 = nn.Conv2d(in_channels=num_hiddens//2,
+                                 out_channels=num_hiddens,
+                                 kernel_size=4,
+                                 stride=2,
+                                 padding=1)
+
+        self._conv_3 = nn.Conv2d(in_channels=num_hiddens,
+                                 out_channels=num_hiddens,
+                                 kernel_size=3,
+                                 stride=1,
+                                 padding=1)
+
+        self._residual_stack = ResidualStack(in_channels=num_hiddens,
+                                             num_hiddens=num_hiddens,
+                                             num_residual_layers=num_residual_layers,
+                                             num_residual_hiddens=num_residual_hiddens)
+
+    def forward(self, x):
+        x = F.relu(self._conv_1(x))
+        x = F.relu(self._conv_2(x))
+        x = self._conv_3(x)
+        return self._residual_stack(x)
