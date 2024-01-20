0711baa7026292ff8e435cbf8ad8509a275040b4
Xin Li <helloimlixin@gmail.com>
2024-01-20 14:08:40 -0500

    Added main training script.

---
+++ main.py
@@ -0,0 +1,135 @@
+#  ==============================================================================
+#  Description: Helper functions for the model.
+#  Copyright (C) 2024 Xin Li
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#  ==============================================================================
+
+import torch
+import torch.nn.functional as F
+from torch.utils.tensorboard import SummaryWriter
+
+from dataloaders.cifar10 import get_cifar10_train_loader, get_cifar10_test_loader
+from models.vqvae import VQVAE
+
+import time
+
+# hyperparameters
+train_batch_size = 256
+test_batch_size = 32
+num_training_updates = 20000
+
+num_hiddens = 128
+num_residual_hiddens = 32
+num_residual_layers = 2
+
+embedding_dim = 64
+num_embeddings = 512
+
+commitment_cost = 0.25
+
+decay = 0
+
+learning_rate = 1e-3
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# data loaders
+train_loader = get_cifar10_train_loader(batch_size=train_batch_size)()
+test_loader = get_cifar10_test_loader(batch_size=test_batch_size)()
+
+# model
+model = VQVAE(in_channels=3,
+              num_hiddens=num_hiddens,
+              num_residual_layers=num_residual_layers,
+              num_residual_hiddens=num_residual_hiddens,
+              num_embeddings=num_embeddings,
+              embedding_dim=embedding_dim,
+              commitment_cost=commitment_cost,
+              decay=decay)
+
+# optimizer
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, amsgrad=False)
+
+# loss function
+def loss_function(recon_x, x):
+    recon_error = F.mse_loss(recon_x, x) / train_loader.data_variance
+    return recon_error
+
+def train():
+    '''Train the model.'''
+    model.train() # set the model to training mode
+    writer = SummaryWriter() # create a writer object for TensorBoard
+
+    for i in range(num_training_updates):
+        optimizer.zero_grad()
+
+        # sample the mini-batch
+        (x,_) = next(iter(train_loader))
+        x = x.to(device)
+
+        # forward pass
+        vq_loss, data_recon, perplexity, encodings = model(x)
+        recon_error = loss_function(data_recon, x)
+        loss = recon_error + vq_loss # total loss
+
+        # backward pass
+        loss.backward()
+
+        # update parameters
+        optimizer.step()
+
+        # print training information
+        if (i+1) % 100 == 0:
+            print('%d iterations' % (i+1))
+            print('recon_error: %.3f' % recon_error.item())
+            print('vq_loss: %.3f' % vq_loss.item())
+            print()
+
+        # write to TensorBoard
+        writer.add_scalar('loss/recon_error', recon_error.item(), i+1)
+        writer.add_scalar('loss/vq_loss', vq_loss.item(), i+1)
+        writer.add_scalar('loss/loss', loss.item(), i+1)
+
+        # write images to TensorBoard
+        if (i+1) % 100 == 0:
+            writer.add_images('data_recon', data_recon, i+1)
+            writer.add_images('data', x, i+1)
+
+        # write model parameters to TensorBoard
+        if (i+1) % 100 == 0:
+            for name, param in model.named_parameters():
+                writer.add_histogram(name, param.clone().cpu().data.numpy(), i+1)
+
+        # write embeddings to TensorBoard
+        if (i+1) % 100 == 0:
+            writer.add_embedding(model._vq_bottleneck._embedding.weight.clone().cpu().data, global_step=i+1)
+
+        # write model to TensorBoard
+        if (i+1) % 100 == 0:
+            writer.add_graph(model, data_recon)
+
+        # write model to ONNX
+        if (i+1) % 100 == 0:
+            torch.onnx.export(model, data_recon, 'vqvae.onnx', verbose=True)
+
+        # write model to TorchScript
+        if (i+1) % 100 == 0:
+            torch.jit.save(torch.jit.script(model), 'vqvae.pt')
+
+if __name__ == '__main__':
+    start = time.time()
+    train()
+    end = time.time()
+    print('Training time: %f seconds.' % (end-start))
+
