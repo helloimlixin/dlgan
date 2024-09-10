45b387fb5451b0727a3260fbdc7aca2370699c46
Xin Li <helloimlixin@gmail.com>
2024-09-10 12:25:52 -0400

    training scripts.

---
+++ train_vqgan.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:4
+#SBATCH --requeue
+#SBATCH --job-name=xl598-vqgan
+#SBATCH --nodes=1
+#SBATCH --mem=16000
+#SBATCH --time=05:00:00
+#SBATCH --output=test.out
+#SBATCH --error=test.err
+
+srun nvidia-smi
+srun python vqgan_training.py
+
